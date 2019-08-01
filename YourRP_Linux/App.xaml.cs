6abf15cddb06c2092ef922a59c53a983a1e002e8
epacik <epaciatko@gmail.com>
2019-08-02 01:29:26 +0200

    Linux version have a proper UI

---
+++ YourRP_Linux/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace YourRP_Linux
+{
+    public class App : Application
+    {
+        public const string DLL = "libdiscord-rpc.so";
+
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ YourRP_Linux/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Windows.Input;
+
+namespace YourRP_Linux
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+           
+        }
+
+        private void Testowo(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+    }
+}
