19b65cb7b0e11b66e657224b27deeb5b83bb07d9
epacik <epaciatko@gmail.com>
2019-08-03 09:27:29 +0200

    Understanding Avalonia UI

--- YourRP_Linux/MainWindow.xaml.cs
+++ YourRP_Linux/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System;
 using System.Windows.Input;
 
 namespace YourRP_Linux
@@ -13,8 +14,30 @@ namespace YourRP_Linux
 #if DEBUG
             this.AttachDevTools();
 #endif
-
+            AddEventHandlers();
            
+        }
+
+        private void AddEventHandlers()
+        {
+            this.Get<Button>("ToggleButton").Click += TogglePresence_Click;
+            this.Get<Button>("SaveButton").Click += SavePresence_Click;
+            this.Get<Button>("RefreshButton").Click += RefreshPresence_Click;
+        }
+
+        private void RefreshPresence_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+
+        }
+
+        private void SavePresence_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+
+        }
+
+        private void TogglePresence_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            
         }
 
         private void Testowo(object sender, Avalonia.Interactivity.RoutedEventArgs e)
@@ -27,5 +50,6 @@ namespace YourRP_Linux
             AvaloniaXamlLoader.Load(this);
         }
 
+       
     }
 }
