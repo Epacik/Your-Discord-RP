65544e75d050059553603350dc2d3bdcd666d327
epacik <epaciatko@gmail.com>
2020-02-27 00:48:48 +0100

    MainWindow and Buttons updates

--- NewYourDiscordRpc/MainWindow.xaml.cs
+++ NewYourDiscordRpc/MainWindow.xaml.cs
@@ -35,12 +35,43 @@ namespace NewYourDiscordRpc
 
             void Toolbar()
             {
+                //Get Buttons on toolbar
                 StartButton = this.Get<Button>("StartButton");
                 StopButton = this.Get<Button>("StopButton");
                 RefreshButton = this.Get<Button>("RefreshButton");
+                SaveButton = this.Get<Button>("SaveButton");
+
+                //Apply events to buttons
+                StartButton.Click += StartButton_Click;
+                StopButton.Click += StopButton_Click;
+                RefreshButton.Click += RefreshButton_Click;
+                SaveButton.Click += SaveButton_Click;
+
+
+                void StartButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+                {
+                    StartButton.IsVisible = false;
+                    StopButton.IsVisible = true;
+                }
+
+                void StopButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+                {
+                    StartButton.IsVisible = true;
+                    StopButton.IsVisible = false;
+                }
+
+                void RefreshButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+                {
+                }
+
+                void SaveButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+                {
+                }
             }
 
         }
+
+        
 
         private void InitializeComponent()
         {
